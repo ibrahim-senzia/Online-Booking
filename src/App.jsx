89802f5ba5ec92633efa82a3d591a6d86f8eb989
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-05-08 09:02:58 +0300

    upgraded

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' 
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import './index.css';
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    
     <Header />
     <Footer />
+  
     
     </>
   )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Header.css';
 
 const Header = () => {
+  const moviePosters = [
+    'src/images/2208.q703.031.F.m004.c7.FP movie film cinema poster banner.jpg',
+    'src/images/148520819_10480861.jpg',
+  ];
+
+  const [currentPoster, setCurrentPoster] = useState(0);
+
+  const handleClick = () => {
+    setCurrentPoster((currentPoster + 1) % moviePosters.length);
+  };
+
   return (
     <>
-    <header>
-      <h1>(GIF)-Movie Ticket Booking </h1>
-      <h4>GIF is an abbreviation derived from the project coordinaters</h4>
-    </header>
+      <header className="header" key="header">
+        <div className="header-content">
+          <h1>(GIF)-Movie Ticket Booking</h1>
+          <h4 className="delayed-text">GIF is an abbreviation derived from the project coordinators</h4>
+        </div>
+      </header>
+      <div className="image-grid" key="image-grid">
+        {moviePosters.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            alt={`Image ${index + 1}`}
+            className="image"
+          />
+        ))}
+      </div>
+      <button onClick={handleClick}>An example of a movie that can be watched after the booking is done</button>
+      <video width="100%" controls>
+        <source src="src/images/Trap _ Official Trailer (720p).mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </>
   );
 };
